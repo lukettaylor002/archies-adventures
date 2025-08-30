c677e2793fb4a4618f51472263615db4e65e08f0
lukettaylor002 <luketaylor002@gmail.com>
2025-08-30 23:51:31 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("src/css");
 
+  // 👉 ADD THIS: a proper "posts" collection (all markdown in /src/posts)
+  eleventyConfig.addCollection("posts", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("src/posts/*.md").sort((a,b) => {
+      return a.date - b.date; // oldest → newest (we reverse in templates)
+    });
+  });
+
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
     markdownTemplateEngine: "njk",
